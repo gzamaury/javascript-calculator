72d2c27ce8bc69bc8632f3eb257b2cd9fd3da229
Amaury Gz <gzamaury@hotmail.com>
2022-12-07 20:18:58 +0000

    Add: Basic Layout

---
+++ src/component/Calculator/Calculator.js
@@ -0,0 +1,14 @@
+import "./Calculator.css";
+
+function Calculator() {
+  return (
+    <div className="cal-container">
+      <div id="calculator">
+        <div id="display">display</div>
+        <div id="kb-left">kb-left</div>
+        <div id="kb-right">kb-right</div>
+      </div>
+    </div>
+  );
+}
+export default Calculator;
