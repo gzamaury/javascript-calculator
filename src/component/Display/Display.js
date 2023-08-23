3b55706540e7ac93709e567c44f8acd14d0edc39
Amaury Gz <gzamaury@hotmail.com>
2023-08-23 04:11:22 +0000

    Add: Clear button

--- src/component/Display/Display.js
+++ src/component/Display/Display.js
@@ -4,7 +4,16 @@ import "./Display.css";
 function Display({ displayId }) {
   return (
     <div className="display-container">
-      <div id={displayId}>display</div>
+      <div id={displayId} className="display">
+        <div id="dsp-main" className="dsp-main">
+          main
+        </div>
+        <div id="dsp-bar-btn" className="dsp-bar-btn">
+          <div id="clear" className="bar-btn-item bar-btn-disabled">
+            CLR
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
