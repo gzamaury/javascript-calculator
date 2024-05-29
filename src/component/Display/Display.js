fc680b4a1096757506039e4e69b0e32e248fb111
Amaury Gz <gzamaury@hotmail.com>
2024-05-29 06:15:57 +0000

    add: Display history and current Operation

--- src/component/Display/Display.js
+++ src/component/Display/Display.js
@@ -1,17 +1,49 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
+import { buildOperation, buildHistory } from "./operationBuilder";
 import "./Display.css";
 
-function Display({ displayId }) {
+function Display({ displayId, history, currentInput }) {
+  const [showHistory, setShowHistory] = useState(false);
+  const isHistoryEmpty = history.length === 0 || history[0].length < 1;
+  const historyBtnClass = isHistoryEmpty
+    ? " bar-btn-disabled"
+    : " bar-btn-enabled";
+  const displayOutput = showHistory
+    ? buildHistory(history)
+    : buildOperation(currentInput);
+
+  const handleClickHistory = () => {
+    setShowHistory(!showHistory);
+  };
+
   return (
-    <div className="display-container">
+    <div className="display-container" data-testid="display">
       <div id={displayId} className="display">
         <div id="dsp-main" className="dsp-main">
-          main
+          <div id="dsp-content" className="dsp-content">
+            {displayOutput}
+          </div>
         </div>
         <div id="dsp-bar-btn" className="dsp-bar-btn">
-          <div id="clear" className="bar-btn-item bar-btn-disabled">
+          <button
+            id="clear"
+            className="bar-btn-item bar-btn-disabled"
+            type="button"
+          >
             CLR
-          </div>
+          </button>
+          <button
+            id="history"
+            name="history"
+            className={`bar-btn-item${historyBtnClass}`}
+            onClick={handleClickHistory}
+            onKeyUp={handleClickHistory}
+            type="button"
+            disabled={isHistoryEmpty}
+          >
+            {!showHistory ? "HISTORY" : "CLOSE HISTORY"}
+          </button>
         </div>
       </div>
     </div>
@@ -20,6 +52,12 @@ function Display({ displayId }) {
 
 Display.propTypes = {
   displayId: PropTypes.string.isRequired,
+  history: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.string)),
+  currentInput: PropTypes.arrayOf(PropTypes.string),
+};
+Display.defaultProps = {
+  history: [[]],
+  currentInput: ["0"],
 };
 
 export default Display;
